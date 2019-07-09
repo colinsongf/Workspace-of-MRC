ae5d1571fbbf29cd645f10c0c07cea747d3ee8f7
Apollo2mars <Apollo2mars@gmail.com>
2019-07-09 22:09:28 +0800

    cleanup utils

---
+++ utils/Tokenizer.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# file: data_utils.py
+# author: apollo2mars <apollo2mars@gmail.com>
+
+import os
+import pickle
+import numpy as np
+import tensorflow as tf
+
+
+def build_tokenizer(fnames, max_seq_len, dat_fname):
+    if os.path.exists(dat_fname):
+        print('loading tokenizer:', dat_fname)
+        tokenizer = pickle.load(open(dat_fname, 'rb'))
+    else:
+        text = ''
+        for fname in fnames:
+            fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+            lines = fin.readlines()
+            fin.close()
+            for i in range(0, len(lines), 3):
+                text_raw = lines[i].lower().strip()
+                text += text_raw + " "
+
+        tokenizer = Tokenizer(max_seq_len)
+        tokenizer.fit_on_text(text)
+        pickle.dump(tokenizer, open(dat_fname, 'wb'))
+    return tokenizer
+
+
+def _pad_and_truncate(sequence, maxlen, dtype='int64', padding='post', truncating='post', value=0):
+    x = (np.ones(maxlen) * value).astype(dtype)
+    if truncating == 'pre':
+        trunc = sequence[-maxlen:]
+    else:
+        trunc = sequence[:maxlen]
+    trunc = np.asarray(trunc, dtype=dtype)
+    if padding == 'post':
+        x[:len(trunc)] = trunc
+    else:
+        x[-len(trunc):] = trunc
+    return x
+
+
+class Tokenizer(object):
+    def __init__(self, max_seq_len, lower=True):
+        self.lower = lower
+        self.max_seq_len = max_seq_len
+        self.word2idx = {}
+        self.idx2word = {}
+
+    def fit_on_text(self, text):
+        if self.lower:
+            text = text.lower()
+
+        from collections import Counter
+        count = Counter(text)
+
+        for idx, item in enumerate(count):
+            self.word2idx[item] = idx + 1 # must + 1
+            self.idx2word[idx + 1] = item
+
+    def text_to_sequence(self, text, reverse=False, padding='post', truncating='post'):
+        if self.lower:
+            text = text.lower()
+        words = list(text)
+        unknownidx = len(self.word2idx)+1
+        sequence = [self.word2idx[w] if w in self.word2idx else unknownidx for w in words]
+        
+        if len(sequence) == 0:
+            sequence = [0]
+        if reverse:
+            sequence = sequence[::-1]
+        
+        return _pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
+
+
