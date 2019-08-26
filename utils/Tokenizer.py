88b0000e7c2ba0427ab1420bd7fabb3079f261fc
Apollo2mars <Apollo2mars@gmail.com>
2019-08-26 13:26:25 +0800

    update utils/Tokenizer.py for staticmethod, classmethod, general method and _ protected, __private

--- utils/Tokenizer.py
+++ utils/Tokenizer.py
@@ -6,6 +6,7 @@ import os
 import pickle
 import numpy as np
 
+
 class Tokenizer(object):
 	""" Tokenizer for Machine Reading Comprehension
 
@@ -16,47 +17,102 @@ class Tokenizer(object):
 		else build new embedding matrix
 	"""
 
-	def __init__(self, max_seq_len, lower=True, emb_type="tencent"):
-		self.lower = lower
+	def __init__(self, origin_text, max_seq_len, emb_dim, lower=True, emb_type="tencent", dat_fname="path_1", fname="path_2"):
+		self.origin_text = origin_text
 		self.max_seq_len = max_seq_len
+		self.emb_dim = emb_dim
+		self.lower = lower
 		self.emb_type = emb_type
+		self.path_1 = dat_fname
+		self.path_2 = fname
+
 		self.word2idx = {}
 		self.idx2word = {}
+		self.vocab_embed = []
 
-        slef.embedding_info = self.__embedding_info()
+		self.embedding_info = Tokenizer.__embedding_info()
+		self.__load_embedding(word2idx=self.word2idx, emb_dim=self.emb_dim, dat_fname=self.path_1, fname=self.path_2)
+		self.__set_vocabulary(self.origin_text)
+		self.__encode_vocab()
 
-        self.embedding_matrix = self.__load_embedding_matrix()
-		self.__get_vocabulary_word_list()
-		self.__get_vocabulary_embeeding_vector_list()
 
-    def __embedding_info(self):
-        embedding_files = {
-            'Static':{
-                "Word2Vec":"",
-                "Glove":"",
-                "Tencent":""},
-            'Dynamic':{
-                "BERT":"",
-                "ELMo":"",
-                "ERINE":"",
-                "GPT-2-Chinese":""
-                "BERT-WWW":""}
-        }
+	@staticmethod
+	def __embedding_info():
+		embedding_files = {
+			'Static':{
+				"Word2Vec":"",
+				"Glove":"",
+				"Tencent":""},
+			'Dynamic':{
+				"BERT":"",
+				"ELMo":"",
+				"ERINE":"",
+				"GPT-2-Chinese":"",
+				"BERT-WWW":""}
+		}
 
-	def __load_embedding_matrix(self, word2idx, embed_dim, dat_fname, fname):
+		return embedding_files
+
+	def __load_embedding(self, word2idx, emb_dim, dat_fname, fname):
 		if os.path.exists(dat_fname):
 			embedding_matrix = pickle.load(open(dat_fname, 'rb'))
 		else:
-			embedding_matrix = np.zeros((len(word2idx) + 2, embed_dim))  # idx 0 and len(word2idx)+1 are all-zeros
-			word_vec = _load_word_vec(fname, word2idx=word2idx)
+			embedding_matrix = np.zeros((len(word2idx) + 2, emb_dim))  # idx 0 and len(word2idx)+1 are all-zeros
+			word_vec = Tokenizer.__get_vocabulary_embedding_vector_list(fname, word2idx=word2idx)
 			for word, i in word2idx.items():
 				embedding_matrix[i] = word_vec[word]
 			pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
 
-		return embedding_matrix
+		self.embedding_matrix = embedding_matrix
 
-	@staticmethod
-	def __pad_and_truncate(self, sequence, maxlen, dtype='int64', padding='post', truncating='post', value=0):
+	def __set_vocabulary(self, input_text):
+		"""
+		:param text: text for generate vocabulary
+		:return: null
+		"""
+		if self.lower:
+			tmp_text = input_text.lower()
+
+		from collections import Counter
+		count = Counter(tmp_text)
+
+		for idx, item in enumerate(count):
+			self.word2idx[item] = idx + 1  # must + 1
+			self.idx2word[idx + 1] = item
+
+	def __encode_vocab(self, input_path, word2idx=None):
+		"""
+		:param input_path: staic embedding file, for example("Glove.5b.300d")
+				, [0.2,0.6,..,0.2]
+				Apple [0.3,0.3,..,0.7]
+				Bob [0.3,0.4,..,0.7]
+				Car [0.5,0.4,..,0.7]
+				Do [0.8,0.4,..,0.7]
+				Eat [0.9,0.4,..,0.7]
+				...
+				Zip [0.3,0.6,..,0.7]
+		:param word2idx: vocabulary for current task  [list]
+				input file : Bob Eat Apple
+				[Apple, Eat, Apple]
+		:return: embedding vector list for vocabury
+				[[0.3,0.4,..,0.7]
+				[0.9,0.4,..,0.7]
+				[0.3,0.3,..,0.7]]
+
+		get embeddding vector list from embedding matrix by vovabulary
+
+		"""
+		fin = open(input_path, 'r', encoding='utf-8', newline='\n', errors='ignore')
+		word_vec = {}
+		for line in fin:
+			tokens = line.rstrip().split(' ')
+			if word2idx is None or tokens[0] in word2idx.keys():
+				word_vec[tokens[0]] = np.asarray(tokens[1:], dtype='float32')
+
+		self.vocab_embed = word_vec
+
+	@classmethod
+	def __pad_and_truncate(cls, sequence, maxlen, dtype='int64', padding='post', truncating='post', value=0):
 		x = (np.ones(maxlen) * value).astype(dtype)
 
 		if truncating == 'pre':
@@ -69,73 +125,26 @@ class Tokenizer(object):
 		else:
 			x[-len(trunc):] = trunc
 		return x
-    
-	def __get_vocabulary_word_list(self, text):
-        """
-        :param text: text for generate vocabulary
-        :return: null
-        """
-		if self.lower:
-			text = text.lower()
-
-		from collections import Counter
-		count = Counter(text)
-
-		for idx, item in enumerate(count):
-			self.word2idx[item] = idx + 1 # must + 1
-			self.idx2word[idx + 1] = item
-
-	def __get_vocabulry_embedding_vator_list(input_path, word2idx=None):
-		"""
-		:param input_path: staic embedding file, for example("Glove.5b.300d")
-                , [0.2,0.6,..,0.2]
-                Apple [0.3,0.3,..,0.7]
-                Bob [0.3,0.4,..,0.7]
-                Car [0.5,0.4,..,0.7]
-                Do [0.8,0.4,..,0.7]
-                Eat [0.9,0.4,..,0.7]
-                ...
-                Zip [0.3,0.6,..,0.7]
-		:param word2idx: vocabulary for current task  [list]
-                input file : Bob Eat Apple
-                [Apple, Eat, Apple]
-		:return: embedding vector list for vocabury
-                [[0.3,0.4,..,0.7]
-                [0.9,0.4,..,0.7]
-                [0.3,0.3,..,0.7]]
-
-        get embeddding vector list from embedding matrix by vovabulary
-		"""
-		fin = open(input_path, 'r', encoding='utf-8', newline='\n', errors='ignore')
-		word_vec = {}
-		for line in fin:
-			tokens = line.rstrip().split(' ')
-			if word2idx is None or tokens[0] in word2idx.keys():
-				word_vec[tokens[0]] = np.asarray(tokens[1:], dtype='float32')
-
-			return word_vec
 
 	def encode(self, text, reverse=False, padding='post', truncating='post'):
-        """
-        convert text to numberical gigital features with max length, paddding
-        and truncating
-        """
+		"""
+		convert text to numberical gigital features with max length, paddding
+		and truncating
+		"""
 		if self.lower:
 			text = text.lower()
 		words = list(text)
-		unknownidx = len(self.word2idx)+1
-		sequence = [self.word2idx[w] if w in self.word2idx else unknownidx for w in words]
-        if len(sequence) == 0:
+		unknown_idx = len(self.word2idx)+1
+		sequence = [self.word2idx[w] if w in self.word2idx else unknown_idx for w in words]
+		if len(sequence) == 0:
 			sequence = [0]
 		if reverse:
 			sequence = sequence[::-1]
 
-		return _pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
+		return Tokenizer.__pad_and_truncate(sequence, self.max_seq_len, padding=padding, truncating=truncating)
 
 
 if __name__ == '__main__':
-    tokenizer = Tokenizer(512, emb_type='tencent')
-
-    text = "中文自然语言处理，Natural Language Process"
-
-    print(Tokenizer.encode(text))
+	tokenizer = Tokenizer(512, emb_type='tencent')
+	text = "中文自然语言处理，Natural Language Process"
+	print(tokenizer.encode(text))
