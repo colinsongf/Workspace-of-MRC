d8f306474626ef2bb1b561ebf2d26d7aef5dc7c4
Apollo2Mars <Apollo2Mars@gmail.com>
2019-09-02 20:22:54 +0800

    update

--- utils/Tokenizer.py
+++ utils/Tokenizer.py
@@ -20,26 +20,35 @@ class Tokenizer(object):
         if embedding matrix exits, load from exit file
         else build new embedding matrix
     """
-    def __init__(self, max_seq_len, emb_type, dat_fname):
+    def __init__(self, origin_file, max_seq_len, emb_type, dat_fname):
+
         self.max_seq_len = max_seq_len
         self.emb_type = emb_type.lower()
         self.dat_path = dat_fname
 
+        self.lower = True
+
+        lines = open(origin_file, 'r', encoding='utf-8', newline='\n', errors='ignore')
+        self.fit_text = ' '.join(lines)
+
+        self.embedding_info = {}
         self.word2idx = {}
         self.idx2word = {}
         self.word2vec = {}  # {"Apple":[1,2,2,3,5], "Book":"1,2,1,1,1"}
         self.embedding_matrix = []
 
-        self.embedding_info = Tokenizer.__embedding_info()
-        self.__load_embedding(word2idx=self.word2idx, dat_fname=self.dat_path)
+        self.__set_embedding_info()
+        self.__set_vocabulary(input_text=self.fit_text)
+        self.__set_word2vec(embedding_path=self.embedding_files['Static'][self.emb_type],
+                            word2idx=self.word2idx)
+        self.__set_embedding_matrix(word2idx=self.word2idx, dat_fname=self.dat_path)
         
-    @staticmethod
-    def __embedding_info():
+    def __set_embedding_info(self):
         embedding_files = {
             'Static':{
                 "Word2Vec":"",
                 "Glove":"",
-                "Tencent":"../resources/Tencent_AILab_ChineseEmbedding.txt"
+                "tencent":"../resources/Tencent_AILab_ChineseEmbedding.txt"
             },
             'Dynamic':{
                 "BERT":"",
@@ -50,31 +59,23 @@ class Tokenizer(object):
             }
         }
         
-        return embedding_files
+        self.embedding_files = embedding_files
 
-    def __load_embedding(self, word2idx, dat_fname):
-        if os.path.exists(dat_fname):
-            print("use exist embedding file")
-            embedding_matrix = pickle.load(open(dat_fname, 'rb'))
-            print(embedding_matrix)
-        elif self.emb_type == 'random':
-            embedding_matrix = np.zeros((len(word2idx) + 2, 300))  # idx 0 and len(word2idx)+1 are all-zeros
-            pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
-        elif self.emb_type == 'tencent':
-            print("************, do tencent embedding")
-            embedding_matrix = np.zeros((len(word2idx) + 2, 200))  # idx 0 and len(word2idx)+1 are all-zeros
-            tmp_path = self.__embedding_info()['Static']["Tencent"]
+    def __set_vocabulary(self, input_text):
+        """
+        :param input_text: text for generate vocabulary
+        :return: null
+        """
+        if self.lower:
+            tmp_text = input_text.lower()
 
-            self.__set_word2vec(embedding_path=tmp_path, word2idx=word2idx)
+        from collections import Counter
+        count = Counter(tmp_text)
 
-            for word, i in word2idx.items():
-                embedding_matrix[i] = self.word2vec[word]
-            pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
-        elif self.emb_type == 'bert':
-            pass
-        
-        self.embedding_matrix = embedding_matrix
-        
+        for idx, item in enumerate(count):
+            self.word2idx[item] = idx + 1  # must + 1
+            self.idx2word[idx + 1] = item
+
     def __set_word2vec(self, embedding_path, word2idx=None):
         """
         :param input_path: staic embedding file, for example("Glove.5b.300d")
@@ -100,14 +101,33 @@ class Tokenizer(object):
         fin = open(embedding_path, 'r', encoding='utf-8', newline='\n', errors='ignore')
         word2vec = {}
         for line in fin:
-            tokens = line.rstrip().split(' ')
+            tokens = line.rstrip().split(' ') 
             if tokens[0] in word2idx.keys():
-                print(tokens[0])
-                print(tokens[1:])
+                print("="*20, "go in")
                 word2vec[tokens[0]] = np.asarray(tokens[1:], dtype='float32')
         
-        print('word2vec', word2vec)
         self.word2vec = word2vec
+
+    def __set_embedding_matrix(self, word2idx, dat_fname):
+        if os.path.exists(dat_fname):
+            print("use exist embedding file")
+            embedding_matrix = pickle.load(open(dat_fname, 'rb'))
+            print(embedding_matrix)
+        elif self.emb_type == 'random':
+            embedding_matrix = np.zeros((len(word2idx) + 2, 300))  # idx 0 and len(word2idx)+1 are all-zeros
+            pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
+        elif self.emb_type == 'tencent':
+            print("************, do tencent embedding")
+            embedding_matrix = np.zeros((len(word2idx) + 2, 200))  # idx 0 and len(word2idx)+1 are all-zeros
+
+            for word, i in word2idx.items():
+                embedding_matrix[i] = self.word2vec[word]
+            pickle.dump(embedding_matrix, open(dat_fname, 'wb'))
+        elif self.emb_type == 'bert':
+            pass
+       
+        self.embedding_matrix = embedding_matrix
+        
 
     def __pad_and_truncate(self, sequence, maxlen, dtype='int64', padding='post', truncating='post', value=0):
         x = (np.ones(maxlen) * value).astype(dtype)
@@ -123,20 +143,6 @@ class Tokenizer(object):
             x[-len(trunc):] = trunc
         return x
 
-    def set_vocabulary(self, input_text):
-        """
-        :param input_text: text for generate vocabulary
-        :return: null
-        """
-        if self.lower:
-            tmp_text = input_text.lower()
-
-        from collections import Counter
-        count = Counter(tmp_text)
-
-        for idx, item in enumerate(count):
-            self.word2idx[item] = idx + 1  # must + 1
-            self.idx2word[idx + 1] = item
 
     def encode(self, text, reverse=False, padding='post', truncating='post'):
         """
@@ -163,10 +169,8 @@ class Tokenizer(object):
 
 if __name__ == '__main__':
     emb_type = 'tencent'
-    tokenizer = Tokenizer(512, emb_type=emb_type, dat_fname=emb_type+"_tokenizer.dat")
+    tokenizer = Tokenizer(origin_file = "a.txt", max_seq_len=512, emb_type=emb_type, dat_fname=emb_type+"_tokenizer.dat")
     text = "中文自然语言处理，Natural Language Process"
-    tokenizer.set_vocabulary(text)
     print(tokenizer.word2idx)
     print(tokenizer.idx2word)
-    print(tokenizer.word2vec)
     print(tokenizer.encode("中文"))
